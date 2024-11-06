4c751bd97429905a88aad67c342fb600cddb1a65
Sahil Saini <sainisahil432@gmail.com>
2024-11-05 22:09:35 -0400

    updated script to save processed images if needed

--- mediapip_hand_recognition.py
+++ mediapip_hand_recognition.py
@@ -1,8 +1,15 @@
+from hmac import new
+import json
 import mediapipe as mp
 import cv2
 import time
 import numpy as np
-from cv2.cuda import GpuMat
+import argparse
+
+# Initialize the parser
+parser = argparse.ArgumentParser("Config")
+parser.add_argument("--saveImage", required=False)
+args = parser.parse_args()
 
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
@@ -20,57 +27,69 @@ cap = cv2.VideoCapture(0)
 CAP_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 CAP_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-if not cap.isOpened():
-    print("Error: could not access the camera.")
-    exit()
-    
+# FUNCTIONS
+
 def writeToFile(filename, data):
     with open(filename, "w") as file:
         for row in data:
-            file.write(" ".join(map(str, row)) + "\n")  
+            file.write(" ".join(map(str, row)) + "\n")
+
+# convert mediapipe image to image compatible with cv2
+def mediapipe_image_to_matlike(mediapipe_image):
+    # convert Mediapipe image to numpy array
+    image_data = mediapipe_image.numpy_view()
+    
+    if mediapipe_image.format == mp.ImageFormat.SRGB:
+        image_array = np.frombuffer(image_data, dtype=np.uint8).reshape((CAP_HEIGHT, CAP_WIDTH, 3))
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
+    elif mediapipe_image.format == mp.ImageFormat.GRAY8:
+        image_array = np.frombuffer(image_data, dtype=np.uint8).reshape((CAP_HEIGHT, CAP_WIDTH))
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+    else:
+        raise ValueError("Unsupported image format")
+    return image_array
 
 # create a gesture recognizer instance with the live stream mode:
-def print_result(result, output_image, timestamp_ms):
-    if output_image is not None:
-        try:
-            image_data = np.array(object=output_image.numpy_view(), dtype=np.uint8)
-            image_bgr = cv2.cvtColor(image_data, cv2.COLOR_RGB2BGR)
-            umat_image = cv2.UMat(image_bgr)
-            # print(f"umat_image -: {umat_image.get()}")
-            cv2.imshow("Gesture_Recognition_Feedback", umat_image.get())
-        except Exception as e:
-            print(f"error showing the gesture recognition frame - {e}")
-    else:
-        print(F"output image not found")
-    
-    # result.gestures = gestures found from the frame
-    # result.handedness = ???
-    # result.hand_landmarks = ??
-    # result.hand_world_landmarks = ??
-    
+def process_result(result, output_image, timestamp_ms):
+    # if anything was found by the frame save the image to disk
+    found_something = False
     if len(result.gestures) > 0:
-        print(f"gestures -: {result.gestures}")
-        
+        found_something = True
+        print(f"Gestures - {result.gestures}")
+
     if len(result.handedness) > 0:
-        print(f"handedness -: {result.handedness}")
+        found_something = True
+        # print(f"Handedness - {result.handedness}")
 
     if len(result.hand_landmarks) > 0:
-        print(f"hand_landmarks -: {result.hand_landmarks}")
+        found_something = True
+        # print(f"Hand Landmarks - {result.hand_landmarks}")
 
     if len(result.hand_world_landmarks) > 0:
-        print(f"hand_world_landmarks -: {result.hand_world_landmarks}")
+        found_something = True
+        # print(f"Hand World Landmarks - {result.hand_world_landmarks}")
+
+    if args.saveImage is not None and found_something is True:
+        try:
+            filename = f"{timestamp_ms}.jpg"
+            cv2.imwrite(filename, output_image.numpy_view())
+        except Exception as e:
+            print(f"error showing the gesture recognition frame - {e}")
+
+if not cap.isOpened():
+    print("Error: could not access the camera.")
+    exit()
 
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result,
+    result_callback=process_result,
 )
 recognizer = GestureRecognizer.create_from_options(options)
 
 # capture frames in a loop
 while True:
     ret,frame = cap.read()
-    # print(f"frame -: {frame}")
     if not ret:
         print("failed to capture video")
         break
@@ -78,11 +97,11 @@ while True:
     # Exit the loop when 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
-    resized_frame = cv2.resize(frame, (IMAGE_WIDTH, IMAGE_HEIGHT))
-    # cv2.imshow("Camera", frame)
 
-    mp_image = mp.Image(mp.ImageFormat.SRGB, resized_frame)
+    # this will show that current frame from the camera
+    cv2.imshow("Camera", frame)
+
+    mp_image = mp.Image(mp.ImageFormat.SRGB, frame)
     mp_timestamp = mp.Timestamp(int(time.time() * 100))
 
     recognizer.recognize_async(mp_image, int(time.time() * 100))
