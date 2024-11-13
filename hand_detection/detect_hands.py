17e3fc262567117ed0ba2380b7545be96b1e0eac
Sahil Saini <sainisahil432@gmail.com>
2024-11-12 21:19:51 -0400

    added code to detect gestures from a single photo

--- hand_detection/detect_hands.py
+++ hand_detection/detect_hands.py
@@ -1,58 +1,95 @@
-from ast import parse
 import mediapipe as mp
 import cv2
-from dataclasses import asdict
+import argparse
+from pathlib import Path
+from pprint import pprint as _print
 
-# open camera and start capturing frames
-cap = cv2.VideoCapture(0)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+from detect_hands_helper import HandLandmark
 
-if not cap.isOpened():
-    print("Error: could not access the camera.")
-    exit()
+# Initialize the parser
+parser = argparse.ArgumentParser("Config")
+parser.add_argument("--live", required=False)
+parser.add_argument("--photo", required=False)
+parser.add_argument("--input", required=False)
+parser.add_argument("--save", required=False)
+args = parser.parse_args()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-print(list(mp_hands.HandLandmark))
+
 with mp_hands.Hands(static_image_mode=False, max_num_hands=2,min_detection_confidence=0.5) as hands:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("failed to capture video")
-            break
+    if args.live == "true":
+        # open camera and start capturing frames
+        cap = cv2.VideoCapture(0)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        if not cap.isOpened():
+            print("Error: could not access the camera.")
+            exit()
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("failed to capture video")
+                break
+            frame = cv2.flip(frame, 1)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            result = hands.process(frame_rgb)
+            
+            processed_hands = []
+            
+            if result.multi_handedness:
+                for handedness in result.multi_handedness:
+                    processed_hands.append(handedness.classification[0].label)
+            
+            if result.multi_hand_landmarks:
+                index = 0
+                for hand_landmarks in result.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
+
+                    wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
+                    thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    
+                    # Display the hand label on the frame
+                    h, w, _ = frame.shape
+                    x, y = int(wrist.x * w), int(wrist.y * h)
+                    cv2.putText(frame, processed_hands[index], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    index = index + 1
+
+            # Display frame
+            cv2.imshow("Hand Detection", frame)
+
+            # Exit the loop when 'q' key is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
+    elif args.photo == "true":
+        if args.input is None:
+            print("missing input image path")
+            exit(0)
+        
+        frame = cv2.imread(args.input)
         frame = cv2.flip(frame, 1)
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = hands.process(frame_rgb)
         
-        processed_hands = []
-        
-        if result.multi_handedness:
-            for handedness in result.multi_handedness:
-                processed_hands.append(handedness.classification[0].label)
-        
-        if result.multi_hand_landmarks:
-            index = 0
-            for hand_landmarks in result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            if args.save == "true":
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                            mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=4),
-                            mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
-
-                wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
-                thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                
-                # Display the hand label on the frame
-                h, w, _ = frame.shape
-                x, y = int(wrist.x * w), int(wrist.y * h)
-                cv2.putText(frame, processed_hands[index], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                index = index + 1
-
-        # Display frame
-        cv2.imshow("Hand Detection", frame)
-
-        # Exit the loop when 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+                            mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=4, circle_radius=8),
+                            mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=4, circle_radius=4))
+        
+        if args.save == "true":
+            # save the update frame
+            filename = Path(args.input).name.split(".")
+            filename.remove("jpg")
+            filename = ".".join(filename)
+            filename = f"./output_images/{filename}-output.jpg"
+            
+            cv2.imwrite(filename, frame)
