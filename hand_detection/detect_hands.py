9de8cb73e094f3a56202c445cbc8506dc3202345
Sahil Saini <sainisahil432@gmail.com>
2024-11-13 20:29:18 -0400

    added function to encode and decode hand landmarks to be saved as metadata

--- hand_detection/detect_hands.py
+++ hand_detection/detect_hands.py
@@ -37,28 +37,15 @@ with mp_hands.Hands(static_image_mode=False, max_num_hands=2,min_detection_confi
             frame = cv2.flip(frame, 1)
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             result = hands.process(frame_rgb)
-            
-            processed_hands = []
-            
-            if result.multi_handedness:
-                for handedness in result.multi_handedness:
-                    processed_hands.append(handedness.classification[0].label)
-            
+
             if result.multi_hand_landmarks:
-                index = 0
                 for hand_landmarks in result.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=4),
                                 mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
 
-                    wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
-                    thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                    
-                    # Display the hand label on the frame
-                    h, w, _ = frame.shape
-                    x, y = int(wrist.x * w), int(wrist.y * h)
-                    cv2.putText(frame, processed_hands[index], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                    index = index + 1
+                    # wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
+                    # thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
 
             # Display frame
             cv2.imshow("Hand Detection", frame)
@@ -75,9 +62,7 @@ with mp_hands.Hands(static_image_mode=False, max_num_hands=2,min_detection_confi
             print("ERROR: missing input image path")
             exit(0)
         
-        if args.save == "true": save = True
-        else: save = False
-        process_image(hands, args.input, save)
+        process_image(hands, args.input, args.save)
 
     elif args.dir is True:
         if args.input is None:
--- hand_detection/detect_hands_helper.py
+++ hand_detection/detect_hands_helper.py
@@ -1,10 +1,16 @@
 from enum import Enum
+import json
+import pickle
+from google.protobuf.json_format import MessageToDict
 import mediapipe as mp
 import cv2
 import argparse
 import os
+import base64
 from pathlib import Path
 from pprint import pprint as _print
+from typing import List, Dict, Any
+from dataclasses import dataclass, asdict, field
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -32,17 +38,67 @@ class HandLandmark(Enum):
     PINKY_DIP = 19
     PINKY_TIP = 20
 
+@dataclass
+class Landmark:
+    x: float
+    y: float
+    z: float
+    
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+@dataclass
+class HandLandmarks:
+    landmarks: List[Landmark] = field(default_factory=list)
+    
+    @classmethod
+    def from_hand_landmarks(self, hand_landmarks):
+        instance = self()
+        hand_landmark_indexes = [landmark.value for landmark in HandLandmark]
+        for index in hand_landmark_indexes:
+            landmark = hand_landmarks.landmark[index]
+            instance.landmarks.append(Landmark(landmark.x,landmark.y,landmark.z))
+        return instance
+    
+    def to_json(self) -> str:
+        return json.dumps(asdict(self))
+    
+    @staticmethod
+    def from_json(json_str: str) -> 'HandLandmarks':
+        data = json.loads(json_str)
+        data['landmarks'] = [Landmark(**landmark) for landmark in data.get('landmarks', [])]
+        return HandLandmarks(**data)
+
 def process_image(hands, inputfile, save):
     frame = cv2.imread(inputfile)
     frame = cv2.flip(frame, 1)
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(frame_rgb)
     
+    processed_hands = []
+    
+    if result.multi_handedness:
+        for handedness in result.multi_handedness:
+            processed_hands.append(handedness.classification[0].label)
+    
+    index = 0
     for hand_landmarks in result.multi_hand_landmarks:
         if save is True:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS,
                 mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=4),
                 mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2))
+            
+            # encoding hand landmarks
+            custom_hand_landmarks = HandLandmarks.from_hand_landmarks(hand_landmarks)
+            encoded_hand_landmarks = base64.b64encode(custom_hand_landmarks.to_json().encode("utf-8")).decode("utf-8")
+            
+            # decoding hand landmarks
+            decoded_string = base64.b64decode(encoded_hand_landmarks).decode("utf-8")
+            decoded_hand_landmarks = HandLandmarks.from_json(decoded_string)
+            _print(decoded_hand_landmarks.landmarks)
+            index = index + 1
     
     if save is True:
         # save the update frame
